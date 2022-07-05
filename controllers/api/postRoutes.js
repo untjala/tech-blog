2ecce06fc42b7765abaa4d9b404136305a7475eb
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-05 15:13:18 -0400

    wrote post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,52 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const post = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const post = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        await Post.destroy(
+            {
+                where: {
+                    id: req.params.id,
+                },
+            });
+
+        res.status(200);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
